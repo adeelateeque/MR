6e485f6532a2cbf846ff61cd5535ba17e0abad31
Adeel Ateeque <adeelateeque@gmail.com>
2014-06-07 01:07:27 +0800

    Project setup

---
+++ src/com/codurs/meetroulette/MapActivity.java
@@ -0,0 +1,16 @@
+package com.codurs.meetroulette;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.codurs.meetroulette.R;
+
+/**
+ * Created by Adeel on 6/7/14.
+ */
+public class MapActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
